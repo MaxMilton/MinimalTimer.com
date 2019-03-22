7b8cc570d9e04c6a48bfb588b3929f7ecb5b9fc1
Max Milton <max@wearegenki.com>
2019-03-23 02:40:23 +1100

    Basic build config

---
+++ externs.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Closure Compiler externs declarations.
+ * @externs
+ */
+
+/* eslint-disable strict, no-var, @typescript-eslint/no-unused-vars, no-underscore-dangle */
+
+// refs in literals
+var count;
+var down;
+var up;
